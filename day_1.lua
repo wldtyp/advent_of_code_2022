638ad62bb1f5447a6351465128beef89852a3d37
andrew <andrew@wildtype.net>
2022-12-02 22:21:23 -0800

    Day 1 part A

--- day_1.lua
+++ day_1.lua
@@ -1,2 +1,15 @@
-input = io.read("*all")
-io.write(input)
+input_file = io.open(arg[1], "r")
+
+max = 0
+current_total = 0
+
+for l in input_file:lines() do 
+	if #l > 0 then
+		current_total = current_total +  l
+	else
+		if current_total > max then max = current_total end
+		current_total = 0
+	end
+end
+print(max)
+
