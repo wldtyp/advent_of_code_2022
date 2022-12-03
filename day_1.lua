184812e8e27328e52bcaa52505e9c66322376331
andrew <andrew@wildtype.net>
2022-12-02 22:49:31 -0800

    Day 1 part B

--- day_1.lua
+++ day_1.lua
@@ -3,13 +3,23 @@ input_file = io.open(arg[1], "r")
 max = 0
 current_total = 0
 
+totals = {}
+
 for l in input_file:lines() do 
 	if #l > 0 then
 		current_total = current_total +  l
 	else
-		if current_total > max then max = current_total end
+		totals[#totals + 1] = current_total
 		current_total = 0
 	end
 end
-print(max)
+
+table.sort(totals, function(a, b) return a > b end)
+
+print(totals[1] + totals[2] + totals[3])
+
+-- for k,v in pairs(totals) do
+-- 	print(k,v)
+-- end
+
 
